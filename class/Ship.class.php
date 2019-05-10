a16a5aa730953c3f0e6bfa3e34858f9899556ac8
Berta Turcotte <bturcott@ox-t2.21-school.ru>
2019-05-10 18:34:50 +0300

    intersects

--- class/Ship.class.php
+++ class/Ship.class.php
@@ -66,6 +66,17 @@ class Ship extends MapObject
 EOF;
     }
 
+    public function intersects($obj2){
+        $top_left1 = $this->pos['top-left'];
+        $top_left2 = $obj2->pos['top-left'];
+        $bottom_right1 = $this->pos['bottom-right'];
+        $bottom_right2 = $this->pos['bottom-right'];
+        return ($top_left1['y'] <$bottom_right2['y'] ||
+        $bottom_right1['y'] > $top_left1['y'] ||
+        $bottom_right1['x'] < $top_left2['x'] ||
+        $top_left1['x'] > $bottom_right2['x']);
+    }
+
     /**
      * @return integer
      */
